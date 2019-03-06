74c512e1cb99dcae73467be9600c0fde70f9d39b
Miriam Rodriguez <miirii2394@gmail.com>
2019-03-06 08:43:10 +0000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,6 @@ gulp.task('default', function(done) {
 
 gulp.task("nc", shell.task("nc localhost 60300"));
 
-gulp.task("nc -U", shell.task("nc -U /tmp/watcher.sock"));
+gulp.task("-U", shell.task("nc -U /tmp/watcher.sock"));
 
 gulp.task("node", shell.task("node net-watcher-json-client.js"));
