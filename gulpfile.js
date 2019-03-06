cbe82a5d0bce035a712cb7eddf67cda887f96553
Miriam Rodriguez <miirii2394@gmail.com>
2019-03-06 08:16:13 +0000

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,8 @@ gulp.task('default', function(done) {
     done();
 });
 
-gulp.task("get", shell.task("curl http://localhost:8000/file.txt"));
+gulp.task("nc", shell.task("nc localhost 60300"));
 
-gulp.task("put", shell.task("curl -X PUT -d hello http://localhost:8000/file.txt"));
+gulp.task("nc -U", shell.task("nc -U /tmp/watcher.sock"));
 
-gulp.task("delete", shell.task("curl -X DELETE http://localhost:8000/file.txt"));
-
-gulp.task("mkcol", shell.task("curl -X MKCOL http://localhost:8000/file.txt"));
-
-gulp.task("doc", shell.task("documentation serve file-system-promises.js -f md"));
-
-gulp.task("repl", shell.task("curl http://localhost:8000/../"));
+gulp.task("node", shell.task("node net-watcher-json-client.js"));
